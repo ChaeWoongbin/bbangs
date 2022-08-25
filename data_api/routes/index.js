2e3206b3dceea724c16c88613ea3f7e6a597dbe7
N-225\N-225 <jerpi053@gmail.com>
2022-08-25 09:50:15 +0900

    server get 동작 확인

--- data_api/routes/index.js
+++ data_api/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'server test' });
 });
 
 module.exports = router;
+
+// get 코드 추가
+router.get('/api/get/test', function(req, res, next) {
+  res.status(200).json({
+    "message" : "api get 동작 테스트 화면"
+  });  
+});
